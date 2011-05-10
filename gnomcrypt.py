62081293eed524da622deb787f4193dc17f7ba3b
Visgean Skeloru <Visgean@gmail.com>
2011-05-10 15:01:51 +0200

    ...

--- gnomcrypt.py
+++ gnomcrypt.py
@@ -2,8 +2,18 @@
 # -*- coding: UTF-8 -*-
 
 # @author: 	Visgean Skeloru 
-# email: 	<visgean@gmail.com>
-# jabber: 	<visgean@jabber.org>
+# email: 	<visgean@gmail.com
+# jabber: 	<visgean@jabber.org
 # github: 	http://github.com/Visgean
+
+
+import aescrypt
+import sys
+
+import gtk
+import pygtk
+
+pygtk.require("2.0")
+
 
 
