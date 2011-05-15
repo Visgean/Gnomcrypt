ad82ebeee661277e57cd9f323f130674856ed4b1
Visgean Skeloru <Visgean@gmail.com>
2011-05-15 02:06:15 +0200

    Finally, working version

---
+++ README.markdown
@@ -0,0 +1,34 @@
+  ____                                             _   
+ / ___|_ __   ___  _ __ ___   ___ _ __ _   _ _ __ | |_ 
+| |  _| '_ \ / _ \| '_ ` _ \ / __| '__| | | | '_ \| __|
+| |_| | | | | (_) | | | | | | (__| |  | |_| | |_) | |_ 
+ \____|_| |_|\___/|_| |_| |_|\___|_|   \__, | .__/ \__|
+                                       |___/|_|        
+
+
+What the shell is this?
+=======================
+
+	One day, I have woken up with feeling that my system is missing something.
+	There was no way how to easily decrypt single files with symetric AES crypths.
+	
+	So I have implemented that by my own. That crypt.py lib may be little 
+	random so if you have any ideas how to make it better feel free to say it.
+	
+What do I need to use it?
+=========================
+	- Zenity Framework
+	- Nautilus-actions manager
+	
+How to install it?
+==================
+	- download source
+	- import actions via nautilus-actions-manager
+	- edit path to the executables
+	- you are done.
+	
+How to use it?
+==============
+	When you click on a file, there should be icon with Gnomcrypt label, click it
+	dialog should appear and than Gnomcrypt will encrypt/decrypt file. 
+	(it´s automatically decrypting when file ends with ".aes" or ".taes")
--- gnomcrypt.py
+++ gnomcrypt.py
@@ -7,13 +7,50 @@
 # github: 	http://github.com/Visgean
 
 
-import aescrypt
+import crypt
 import sys
-
-import gtk
-import pygtk
-
-pygtk.require("2.0")
+import subprocess
+import pynotify
+import shlex
 
 
+
+def runCommand(commandString):
+    "Run command outside python"
+    cmd = subprocess.Popen(shlex.split(commandString),
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE
+            )
+    OUT, ERR = cmd.communicate()
+    
+    return OUT
+
+def encrypt():
+    cryptat.encryptFile()
+    notifyString = "%s was encrypted." % filePath
+    notif = pynotify.Notification("Gnomcrypt", notifyString)
+    notif.show()
+
+def decrypt():
+    cryptat.decryptFile()
+    notifyString = "%s was decrypted." % filePath
+    notif = pynotify.Notification("Gnomcrypt", notifyString)
+    notif.show()
+
+
+filePath = " ".join(sys.argv[1:])
+
+
+
+password = runCommand(commandString='''zenity --entry --title="Gnomcrypt" --text="Enter password:" --hide-text --window-icon=/usr/share/icons/gnome-colors-common/scalable/status/locked.svg''')
+password = password.splitlines()[0] # there is for some reason line on the end of the string
+
+cryptat = crypt.FileContainer(filePath, password)
+
+
+
+if filePath[-4:] == ".aes" or filePath[-5:] == ".taes":
+    decrypt()
+else:
+    encrypt()
 
